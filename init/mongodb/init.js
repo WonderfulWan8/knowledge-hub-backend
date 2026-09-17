242f8061b6602999f4efe64d4b181222d5790ac6
Leo Bartholomeu <96806061+WonderfulWan8@users.noreply.github.com>
2026-09-17 20:16:53 +0800

    feat:first_commit

---
+++ init/mongodb/init.js
@@ -0,0 +1,10 @@
+db = db.getSiblingDB("knowledge_hub");
+
+db.createUser({
+  user: "knowledge_hub_user",
+  pwd: "knowledge_hub_password",
+  roles: [{ role: "readWrite", db: "knowledge_hub" }],
+});
+
+db.createCollection("kh_document");
+db.createCollection("kh_chunk");
